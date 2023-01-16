393f2c5415e28032ede0873af9bd43ba0050fd58
Rachel Lee <leerachel481@gmail.com>
2023-01-15 16:35:57 -0800

    Added documentation for member functions

--- RL.hpp
+++ RL.hpp
@@ -9,10 +9,19 @@ const int RL_LEVELS = 3;
 
 class RL {
 public:
+    // Constructor
     RL();
+
+    // Get the currently running process
     int running();
+
+    // Move the currently running process to the end of the priority list corresponding to its priority level
     void timeout();
+    
+    // Add a process to the RL by specifying the process index and its priority level
     void add(int processIndex, int priority);
+
+    // Remove a process from the RL
     void remove(int processIndex);
 
 private:
